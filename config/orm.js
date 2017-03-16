0fad466b71670493cd6a1c9d42637a4dd93f19bc
MelissaLoggins <melissaloggins@gmail.com>
2017-03-16 12:54:47 -0400

    Say hello to my working friend.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,6 @@
 var connection = require('./connection.js');
 
+// Object for SQL statement functions.
 var orm = {
   selectAll: function(cb){
     connection.query("SELECT * FROM burgers", function(err, res){
@@ -38,70 +39,3 @@ module.exports = orm;
 
 
 
-// var connection = require("../config/connection.js");
-
-// // Helper function for SQL syntax.
-// function objToSql(ob) {
-//   var arr = [];
-
-//   for (var key in ob) {
-//     if (Object.hasOwnProperty.call(ob, key)) {
-//       arr.push(key + "=" + ob[key]);
-//     }
-//   }
-
-//   return arr.toString();
-// }
-
-// // Object for all our SQL statement functions.
-// var orm = {
-//   all: function(tableInput, cb) {
-//     var queryString = "SELECT * FROM " + tableInput + ";";
-//     connection.query(queryString, function(err, result) {
-//       if (err) {
-//         throw err;
-//       }
-//       cb(result);
-//     });
-//   },
-//   create: function(table, cols, vals, cb) {
-//     var queryString = "INSERT INTO " + table;
-
-//     queryString += " (";
-//     queryString += cols.toString();
-//     queryString += ") ";
-//     queryString += "VALUES (?)";
-//     // queryString += printQuestionMarks(vals.length);
-//     // queryString += ") ";
-
-//     console.log(queryString);
-
-//     connection.query(queryString, vals, function(err, result) {
-//       if (err) {
-//         throw err;
-//       }
-//       cb(result);
-//     });
-//   },
-//   // An example of objColVals would be {name: panther, sleepy: true}
-//   update: function(table, objColVals, condition, cb) {
-//     var queryString = "UPDATE " + table;
-
-//     queryString += " SET ";
-//     queryString += objToSql(objColVals);
-//     queryString += " WHERE ";
-//     queryString += condition;
-
-//     console.log(queryString);
-//     connection.query(queryString, function(err, result) {
-//       if (err) {
-//         throw err;
-//       }
-
-//       cb(result);
-//     });
-//   }
-// };
-
-// // Export the orm object for the model (cat.js).
-// module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,41 +1,50 @@
-var express = require("express");
-var router = express.Router();
-
-// Import the model (burger.js) to use its database functions.
 var express = require('express');
 var burger = require('../models/burger.js');
 var router = express.Router();
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    
-    var burgers = { burgers: data};
+//Creation and exporting router.
 
-    res.render("index", burgers);
-  });
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+    
+      var burgers = { burgers: data};
+
+      res.render("index", burgers);
+    })
 });
 
 router.post("/", function(req, res) {
     
-    var name = req.body.name;
+      var name = req.body.name;
 
-    burger.create(name, function(data){
-      console.log(data);
-      res.redirect("/");
-    })
+      burger.create(name, function(data){
+        console.log(data);
+        res.redirect("/");
+      })
 });
   
 
 router.put("/:id", function(req, res) {
   
-  var name  = req.params.id;
-  console.log(name);
+      var name  = req.params.id;
+      console.log(name);
 
-  burger.update(name, function(data){
-    console.log(data);
-    res.redirect("/")
-    })
+      burger.update(name, function(data){
+        console.log(data);
+        res.redirect("/")
+      })
+});
+
+router.delete("/:id", function(req, res){
+  
+      var name = req.params.id;
+      console.log(name);
+
+      burger.delete(name, function(data){
+        console.log(data);
+        res.redirect("/");
+      })
+
 });
 
 
